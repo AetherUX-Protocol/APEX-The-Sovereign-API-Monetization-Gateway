a5073fe03592de783e9bd0ad2922cca805df113e
AetherUX-Protocol <ramanarolla@yahoo.in>
2026-04-21 20:27:27 +0530

    Update providers.tsx

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,1 +1,15 @@
+"use client"
+import { PropsWithChildren } from "react"
+import { InterwovenKitProvider, TESTNET } from "@initia/interwovenkit-react"
+import "@initia/interwovenkit-react/dist/index.css" // Import the mandatory styles
 
+export default function Providers({ children }: PropsWithChildren) {
+  return (
+    <InterwovenKitProvider 
+      chainConfig={TESTNET} 
+      enableAutoSign={true} // Highlighting the Session UX feature
+    >
+      {children}
+    </InterwovenKitProvider>
+  )
+}
